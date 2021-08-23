aae07378899a9cdcaf62ca090ac9f92d030828e4
Benjamin Horsleben <benjamin@fizkerinc.dk>
2021-08-23 08:12:17 +0200

    Updated Package.swift to require Swift 5.4 instead of Swift 4

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.4
 
 import PackageDescription
 
 let package = Package(
     name: "Lark",
     products: [
-        .library(name: "Lark", targets: ["Lark"])
+        .library(name: "Lark", targets: ["Lark"]),
+        .executable(name: "lark-generate-client", targets: ["lark-generate-client"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.5.0")
@@ -14,10 +15,9 @@ let package = Package(
         .target(name: "Lark", dependencies: ["Alamofire"]),
         .target(name: "CodeGenerator", dependencies: ["Lark", "SchemaParser"]),
         .target(name: "SchemaParser", dependencies: ["Lark"]),
-        .target(name: "lark-generate-client", dependencies: ["SchemaParser", "CodeGenerator"]),
+        .executableTarget(name: "lark-generate-client", dependencies: ["SchemaParser", "CodeGenerator"]),
         .testTarget(name: "CodeGeneratorTests", dependencies: ["CodeGenerator"]),
         .testTarget(name: "LarkTests", dependencies: ["Lark"]),
         .testTarget(name: "SchemaParserTests", dependencies: ["SchemaParser"])
-    ],
-    swiftLanguageVersions: [4]
+    ]
 )
